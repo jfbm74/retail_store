a65d98869ce89867dff7b926e08bda52d9d822c0
jfbm74 <jfbm74@gmail.com>
2021-04-08 23:10:56 -0500

    Adding User API

---
+++ applications/users/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'email',
+            'name',
+            'last_name',
+            'gov_id',
+            'company',
+        ]
---
+++ applications/users/urls.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+
+from .viewsets import UserViewSet
+
+router = routers.SimpleRouter()
+router.register('users', UserViewSet)
+
+urlpatterns =  router.urls
