86de8c17a22bc33d4413d2d6e8828d68f3da715f
jfbm74 <jfbm74@gmail.com>
2021-04-09 20:48:29 -0500

    Fixing code style in orders application

--- applications/orders/apps.py
+++ applications/orders/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class OrdersConfig(AppConfig):
+    """ This class is required for django to identify a new application"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'applications.orders'
